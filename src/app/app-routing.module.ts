0339596c8dc1b64edd839b46738b9e44a42e8a8c
Obaid Ramadan <obaidramadan0@gmail.com>
2025-02-28 17:52:39 +0200

    make routing and do nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CarsPageComponent } from './components/cars-page/cars-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home',component: HomeComponent},
+  {path:'cars',component: CarsPageComponent},
+  {path:'cars/:id',component: CarsPageComponent},
+  {path:'**',component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { CardDetailsComponent } from './components/card-details/card-details.component';
+import { RentComponent } from './components/rent/rent.component';
+import { ChooseUsComponent } from './components/choose-us/choose-us.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { AppsComponent } from './components/apps/apps.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { CarsPageComponent } from './components/cars-page/cars-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LayoutComponent,
+    CardDetailsComponent,
+    RentComponent,
+    ChooseUsComponent,
+    TestimonialsComponent,
+    AppsComponent,
+    FooterComponent,
+    NotFoundComponent,
+    SigninComponent,
+    SignupComponent,
+    CarDetailsComponent,
+    CarsPageComponent
   ],
   imports: [
     BrowserModule,
